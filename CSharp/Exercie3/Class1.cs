d45a157017c83311fcb7be965432d74dfd132e3f
ervinrojas <ervin_rojas@yahoo.es>
2022-08-29 17:33:25 -0500

    CSharp commit

---
+++ CSharp/Exercie3/Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercie3
+{
+    internal class ClassA
+    {
+        public void lengthString(string word1, string word2)
+        {
+            if (word1.Length > word2.Length)
+            {
+                Console.Write("String One is longer.");
+            }
+            else
+            {
+                Console.Write("String Two is longer.");
+            }
+        }
+
+        public void sizeList(List<String> listString, List<int> listInt)
+        {
+            
+            Console.WriteLine($"\n tamaño de la lista String es: " + listString.Count);
+            Console.WriteLine($"\n tamaño de la lista Int es: " + listInt.Count);
+
+            if (listString.Count > listInt.Count)
+            {
+                Console.Write("List One is longer.");
+            }
+            else
+            {
+                Console.Write("List Two is longer.");
+            }
+                                    
+        }
+    }
+
+    class ClassB:ClassA, IInterface
+    {
+        public void Test()
+        {
+            Console.WriteLine("\n Hello Interface");
+        }
+    }
+
+    class ClassC
+    {
+        public void orderLists(int opc, List<string> listString, List<int> listInt)
+        { 
+            
+
+            if (opc == 1)
+            { 
+                //List<listString> orderList = list.OrderBy(listString => listString.Object).ToList();
+                var orderList = listString.OrderBy(x=>x.Substring(0));
+                foreach (string Item in orderList)
+                Console.WriteLine(Item);
+            }
+            else
+            {
+                //List<listString> orderList = list.OrderByDescending(listString => listString.Object)/*.ThenByDescending(listString => listString.Object).ToList();
+                var orderList = listString.OrderByDescending(x =>x.Substring(0));
+                foreach (string Item in orderList)
+                Console.WriteLine(Item);
+            }
+                        
+        }
+
+        public void Filters(List<int> listInt)
+        {
+            Console.WriteLine("List with duplicates");
+            listInt.ForEach(Console.WriteLine);
+
+            List<int> uniqueList = listInt.Distinct().ToList();
+            Console.WriteLine("List after removing duplicates");
+            uniqueList.ForEach(i => Console.WriteLine($"{i}"));
+            Console.ReadLine();
+        }
+    }
+    
+    class listString
+    {
+        public String Object { get; set; }
+    }
+
+    class listInt
+    {
+        public int Number { get; set; }
+    }
+    
+}
---
+++ CSharp/Exercie3/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercie3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> listString = new List<string>();
+            List<int> listInt = new List<int>();
+
+            listString.Add("Text");
+            listString.Add("Computer");
+            listString.Add("Automation");
+            listString.Add("Visual Studio");
+            listString.Add("Auto Parts");
+
+            listInt.Add(1);
+            listInt.Add(1);
+            listInt.Add(2);
+            listInt.Add(3);
+            listInt.Add(4);
+            listInt.Add(4);
+            listInt.Add(5);
+            listInt.Add(6);
+            listInt.Add(7);
+            listInt.Add(8);
+            listInt.Add(10);
+
+            
+            Console.Write("Give 1. word >");
+            String word1 = Console.ReadLine();
+            Console.Write("Give 2. word >");
+            String word2 = Console.ReadLine();
+
+            ClassA prueba = new ClassA();
+            prueba.lengthString(word1, word2);
+
+            ClassA size = new ClassA();
+            size.sizeList(listString,listInt);
+
+            ClassB inherits = new ClassB();
+            inherits.sizeList(listString,listInt);
+            
+            ClassB Test = new ClassB();
+            Test.Test();
+
+            
+            int opc;
+
+            Console.WriteLine("1. Orden Ascendente");
+            Console.WriteLine("2. Orden Descendente");
+            opc = int.Parse(Console.ReadLine());
+
+            ClassC Order = new ClassC();
+            Order.orderLists(opc,listString,listInt);
+
+            ClassC filter = new ClassC();
+            filter.Filters(listInt);
+            
+        }
+    }
+
+interface IInterface
+    {
+        void Test();
+    }
+
+}
